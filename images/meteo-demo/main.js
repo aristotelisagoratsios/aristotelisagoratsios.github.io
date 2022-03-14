aea8ef2f2868193835f5759e4012f9e7edee8de6
aristotelisagoratsios <agotel@gmail.com>
2022-03-14 10:16:50 +0200

    initial commit

---
+++ images/meteo-demo/main.js
@@ -0,0 +1,18 @@
+<script type="text/javascript">
+
+ $(document).ready(function () {
+            $("span").click(function () {
+            $("#st1").add("#st2").add("#st3").add("#st4")
+            .css("background", "#4169E1");
+            });
+        });
+        
+  
+$(document).ready(function() {
+       $('#st1').add("#st2").add("#st3").add("#st4").click( function() {
+       $(this)
+       .css('background', '#CD853F')
+              });
+            });
+            
+ </script>           
